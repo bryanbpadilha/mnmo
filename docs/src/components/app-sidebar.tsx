39e6e7000b5b04fa123e283b3e41f878d2e869e6
bryanbpadilha <bryan@bryanbpadilha.com>
2026-01-23 19:41:04 -0300

    Add masterform again

---
+++ docs/src/components/app-sidebar.tsx
@@ -0,0 +1,79 @@
+import * as React from "react"
+
+import { SearchForm } from "@/components/search-form"
+import { VersionSwitcher } from "@/components/version-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+const data = {
+  versions: ["0.0.0"],
+  navMain: [
+    {
+      title: "Getting Started",
+      url: "/",
+      items: [
+        {
+          title: "Introduction",
+          url: "/",
+        },
+      ],
+    },
+    {
+      title: "Examples",
+      items: [
+        {
+          title: "Textbox",
+          url: "/examples/textbox",
+        },
+        {
+          title: "Master Form",
+          url: "/examples/masterform",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <VersionSwitcher
+          versions={data.versions}
+          defaultVersion={data.versions[0]}
+        />
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
---
+++ docs/src/components/layout.tsx
@@ -0,0 +1,16 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex-1 px-8 py-10">
+        <SidebarTrigger />
+        <div className="mx-auto max-w-3xl space-y-8">
+          {children}
+        </div>
+      </main>
+    </SidebarProvider>
+  )
+}
