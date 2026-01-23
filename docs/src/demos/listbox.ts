95eec9970ca7bac65d5f9ed67b935a3fc9f1dd36
bryanbpadilha <bryan@bryanbpadilha.com>
2026-01-23 18:49:06 -0300

    feat: Replace vite docs with astro

--- docs/src/demos/listbox.ts
+++
@@ -1,9 +0,0 @@
-import { Listbox } from "../mnmo";
-
-export function initListboxDemo() {
-    const element = document.querySelector(
-        "#demo-listbox"
-    ) as HTMLElement | null;
-    if (!element) return;
-    new Listbox(element);
-}
